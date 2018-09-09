18e32927152e5c9bdd4026644930aabafe9abf84
ms <--->
2018-09-09 16:21:04 +0300

    fixed main file

--- lorem.d/main.go
+++ lorem.d/main.go
@@ -13,36 +13,58 @@ import (
 )
 
 func main() {
+
 	ctx := context.Background()
+
 	errChan := make(chan error)
 
 	var svc microservicetutorial.Service
+
 	svc = microservicetutorial.LoremService{}
+
 	endpoint := microservicetutorial.Endpoints{
+
 		LoremEndpoint: microservicetutorial.MakeLoremEndpoint(svc),
 	}
 
 	// Logging domain.
+
 	var logger log.Logger
+
 	{
+
 		logger = log.NewLogfmtLogger(os.Stderr)
-		logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
-		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
+
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+
+		logger = log.With(logger, "caller", log.DefaultCaller)
+
 	}
 
 	r := microservicetutorial.MakeHttpHandler(ctx, endpoint, logger)
 
 	// HTTP transport
+
 	go func() {
+
 		fmt.Println("Starting server at port 8080")
+
 		handler := r
+
 		errChan <- http.ListenAndServe(":8080", handler)
+
 	}()
 
 	go func() {
+
 		c := make(chan os.Signal, 1)
+
 		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
 		errChan <- fmt.Errorf("%s", <-c)
+
 	}()
+
 	fmt.Println(<-errChan)
+
 }
