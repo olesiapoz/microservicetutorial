3c6f9e171f5a37d913ad4f78ba9a5da8aeba17fd
ms <--->
2018-09-09 14:42:56 +0300

    First commit

---
+++ lorem.d/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-kit/kit/log"
+	lorem "github.com/ru-rocker/gokit-playground/lorem"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	ctx := context.Background()
+	errChan := make(chan error)
+
+	var svc lorem.Service
+	svc = lorem.LoremService{}
+	endpoint := lorem.Endpoints{
+		LoremEndpoint: lorem.MakeLoremEndpoint(svc),
+	}
+
+	// Logging domain.
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
+		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
+	}
+
+	r := lorem.MakeHttpHandler(ctx, endpoint, logger)
+
+	// HTTP transport
+	go func() {
+		fmt.Println("Starting server at port 8080")
+		handler := r
+		errChan <- http.ListenAndServe(":8080", handler)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+	fmt.Println(<-errChan)
+}
